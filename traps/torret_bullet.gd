f180ee5462f58ac5016f7f804829812a35831707
Eric Fritzen Valle <166267631+Unlxckyz@users.noreply.github.com>
2024-10-14 18:31:37 -0300

    kk

--- traps/torret_bullet.gd
+++ traps/torret_bullet.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 	position += direction.normalized() * speed * delta
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is TileMap:
+	if body.is_in_group("colision_wall"):
 		queue_free()
 	if body is CharacterBody2D:
 		if body.is_in_group("Player"):
